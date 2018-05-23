98f2c7c1b0864cb344487b807250887a16013b36
Joris Sparla <joris.sparla@gmail.com>
2018-05-23 11:35:48 +0200

    fix

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const app = express();
+const server = express();
 
 app.get('*', (req, res) => {
   //res.send('Main routes')
   res.redirect('http://nlbavwixs:3000');
 });
 
+server.get('*', (req, res) => {
+  //res.send('Main routes')
+  res.redirect('http://nlbavwixs/ixs');
+});
+
 app.listen(3000, () => console.log('Listening on port 3000'));
+server.listen(80, () => console.log('Server listening at port 80'));
