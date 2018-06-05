43faff2483fb39d41c2c79888fdc4704a8bbea6a
Joris Sparla <joris.sparla@gmail.com>
2018-06-05 11:06:29 +0200

    added dotenv

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 const server = express();
 
+const { SERVER1, SERVER2 } = process.env;
 app.get('*', (req, res) => {
   //res.send('Main routes')
-  res.redirect('http://nlbavwixs:3000');
+  res.redirect(SERVER1 || 'http://nlbavwixs.infor.com:3000');
 });
 
 server.get('*', (req, res) => {
   //res.send('Main routes')
-  res.redirect('http://nlbavwixs/ixs');
+  res.redirect(SERVER2 || 'http://nlbavwixs.infor.com/ixs');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
